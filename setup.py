23582ff71a8baa0bfb9feaefb36a9cbd28c14999
Robin Edwards <robin.ge@gmail.com>
2015-11-19 16:41:48 +0000

    Add basic setup file

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='bouncer',
+    version='0.0.1',
+    description='Client for bouncer A/B testing service',
+    long_description=open('README.rst').read(),
+    author='Robin Edwards',
+    author_email='robin.ge@gmail.com',
+    zip_safe=True,
+    url='http://github.com/robinedwards/bouncerpy',
+    license='MIT',
+    packages=find_packages(),
+    keywords='bouncer',
+    install_requires=['requests==2.8.1'],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        "Programming Language :: Python :: 2.7",
+    ])
